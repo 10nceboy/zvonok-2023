3c8260ec76f548d5da8b6ea561c12b5d7f0e6888
10nceboy <10nceboy@gmail.com>
2023-09-18 10:00:40 +0300

    player done

--- js/redial.js
+++ js/redial.js
@@ -25,12 +25,28 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
+  document.querySelectorAll(".audio-mobile").forEach((audiom) => {
+    audiom.addEventListener("timeupdate", (event) => {
+      let orangeMobileBar = event.currentTarget
+        .closest(".redial-player-wrapper-mobile")
+        .querySelector(".wave-orange");
+
+      let percent = Math.floor((audiom.currentTime / audiom.duration) * 100);
+
+      orangeMobileBar.style.width = `${percent}%`;
+
+      if (percent == 100) {
+        orangeMobileBar.style.width = "0%";
+      }
+    });
+  });
+
   const handleTimeLineClick = (event) => {
     const { target: timeline, clientX } = event;
     const audioa = timeline
       .closest(".redial-player-wrapper")
       .querySelector("audio");
-    console.log(audioa);
+
     const rect = timeline.getBoundingClientRect();
     const x = clientX - rect.left;
     const pcent = Math.floor((x / rect.width) * 100);
@@ -44,4 +60,8 @@ document.addEventListener("DOMContentLoaded", () => {
     .forEach((timeline) => {
       timeline.addEventListener("click", handleTimeLineClick);
     });
+
+  document.querySelectorAll(".wave-wrapper").forEach((timeline) => {
+    timeline.addEventListener("click", handleTimeLineClick);
+  });
 });
