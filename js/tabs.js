bca829dffb6623cfa2732a1a7093c89618b5df5a
stasmyhero <es@finfort.ru>
2023-10-08 22:08:13 +0300

    refactor code: move all common classes for 2023 pages for a single file, use bootstrap utility classes instead of custom, remove unsued classes

---
+++ js/tabs.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  if (document.querySelector(".tabs")) {
+    let lists = document.querySelectorAll(".tabs-list");
+    lists.forEach((el) => {
+      let list = el.closest(".tabs-list");
+      let links = list.querySelectorAll("li[data-box]");
+      links.forEach((el) => {
+        el.addEventListener("click", function (evt) {
+          document
+            .querySelectorAll(`li`)
+            .forEach((link) => link.classList.remove("active"));
+          el.classList.add("active");
+          let box = el.closest(".steps-js").querySelectorAll("[data-box-item]");
+          let attr = evt.currentTarget.dataset.box;
+          box.forEach((pic) => pic.classList.remove("visible", "d-block"));
+          document
+            .querySelector(`[data-box-item="${attr}"]`)
+            .classList.add("visible", "d-block");
+        });
+      });
+    });
+  }
+});
